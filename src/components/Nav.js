75d35a6e42b9bb1188e82f46209023de1a73c1b5
박은성 <cro3179@naver.com>
2023-10-19 18:10:18 +0900

    nav 추가

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+function Nav() {
+  return (
+    <div className='w-full'>
+        <div className="max-w-[1280px] box-border flex items-start justify-between mx-auto py-10 ">
+<div className="flex justify-start md:hidden sm:hidden">Logo</div>
+<ul className="flex justify-between md:hidden sm:hidden">
+<li className="px-9">ABOUT</li>
+<li className="px-9">SKILL</li>
+<li className="px-9">CONTACT</li>
+</ul>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
