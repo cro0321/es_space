13d213e204f3fc9498304b29db392e32abc9b979
박은성 <cro3179@naver.com>
2023-10-20 17:43:05 +0900

    메인 이미지 + 원형 구현

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-[1280px] mt-10'>
+        <p className="">About</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Square.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Square() {
+  return (
+<>
+{/* <div className="border-b-100 border-red border-solid border-transparent border-l-25 border-r-25 h-3.5 w-100"></div> */}
+<div className="w-full h-full">
+    <div className="w-[94%] flex justify-center items-center mx-auto ">
+{/* bg-[url('/Images/Group 12.png')] */}
+<div className="centerbg px-5 relative">
+<div className=" w-[382px] h-[382px] rounded-full absolute opacity-80
+bg-gradient-to-t from-[#fccbff] from-1% via-[#c59ef8] via-35% to-[#e5d3ec] to-80% left-[5%] -bottom-[10%]">
+<p>home</p>
+</div>
+  <div className=" w-[14px] h-[14px] rounded-full absolute opacity-80
+bg-gradient-to-t from-[#fccbff] from-1% via-[#c59ef8] via-35% to-[#e5d3ec] to-80% left-[39.4%] top-[20%]"></div>
+  <div className=" w-[37px] h-[37px] rounded-full absolute opacity-80
+bg-gradient-to-t from-[#fccbff] from-1% via-[#c59ef8] via-35% to-[#e5d3ec] to-80% left-[40.5%] top-[45%]"></div>
+  <div className=" w-[10px] h-[10px] rounded-full absolute opacity-80
+bg-gradient-to-t from-[#fccbff] from-1% via-[#c59ef8] via-35% to-[#e5d3ec] to-80% right-[36%] top-[5%]"></div>
+  <div className=" w-[104px] h-[104px] rounded-full absolute opacity-80
+bg-gradient-to-t from-[#fccbff] from-1% via-[#c59ef8] via-35% to-[#e5d3ec] to-80% right-[18%] bottom-[20%]"></div>
+</div>
+
+</div>
+</div>
+</>
+  )
+}
+
+export default Square
